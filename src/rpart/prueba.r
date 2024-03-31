388ce5fff9f2ec43c313fdf6652f61639f6ff542
Yliana-Mogollon <163745321+Yliana-Mogollon@users.noreply.github.com>
2024-03-31 11:58:27 -0300

    Prueba 1er commit
    Se cambió la semilla.

---
+++ src/rpart/prueba.r
@@ -0,0 +1,16 @@
+# Arbol elemental con libreria  rpart
+# Debe tener instaladas las librerias  data.table  ,  rpart  y  rpart.plot
+
+# cargo las librerias que necesito
+require("data.table")
+require("rpart")
+require("rpart.plot")
+
+# Aqui se debe poner la carpeta de la materia de SU computadora local
+setwd("X:\\gdrive\\labo2024v1\\") # Establezco el Working Directory
+
+# cargo el dataset
+dataset <- fread("./datasets/dataset_pequeno.csv")
+
+dtrain <- dataset[foto_mes == 202107] # defino donde voy a entrenar
+dapply <- dataset[foto_mes == 202109] # defino donde voy a aplicar el modelo
